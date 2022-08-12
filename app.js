5f600c26bb80d311d616607e89f17370a6dd2bed
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-12 10:41:55 -0300

    feat: criado logica de erro generica e configurado o uso da camada de routes

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+require('express-async-errors');
+const routers = require('./routes');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/', routers);
+
+app.use((err, _req, res, _next) => res.status(err.code || 500)
+    .json({ message: err.message }));
 
 // não remova essa exportação, é para o avaliado funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
