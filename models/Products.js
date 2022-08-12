6f2dcce78adce61e1e5a6cdd36ae53db290db677
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-12 09:31:04 -0300

    feat: camada de model do para a rota get products

---
+++ models/Products.js
@@ -0,0 +1,26 @@
+const connection = require('./connection');
+
+const getProductsAll = async () => {
+  const query = 'SELECT id, name FROM StoreManager.products ORDER BY id';
+  const [response] = await connection.query(query);
+  if (!response) {
+    return false;
+  }
+  return response;
+};
+
+const getProductById = async (id) => {
+  const query = `SELECT id, name FROM StoreManager.products
+  WHERE id = ?`;
+  const [response] = await connection.execute(query, [id]);
+  if (!response) {
+    return false;
+  }
+  return response[0];
+};
+
+getProductById(1).then((teste) => console.log(teste));
+module.exports = {
+  getProductsAll,
+  getProductById,
+};
